d4905a40359abc594470a85469b0de9bcb287624
agurgick <agurgick1@gmail.com>
2024-09-14 18:52:40 -0400

    chapter 3

---
+++ chapter 3/guest_list_3_4.py
@@ -0,0 +1,2 @@
+guest_list = ["Chase Utley", "Andy Reid", "Mike Schmidt", "Donald Trump"]
+print(f"Dear {guest_list[0].title()} I would like to invite you to lunch on")
---
+++ chapter 3/names_3_1.py
@@ -0,0 +1,6 @@
+names = ["Adam", "Amanda", "Aiden", "Seth", "Mary", "Merissa"]
+message = f"Hello {names[0].title()}, how are you doing today"
+
+print(message)
+
+
